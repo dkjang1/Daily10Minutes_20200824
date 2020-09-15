55c7b2ccae091a20849e227a8a90dc89251cde07
JangDongKuk <dkjang@naver.com>
2020-09-15 19:23:58 +0900

    firebase-messaging 추가

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/fcm/MyFCMService.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/fcm/MyFCMService.kt
@@ -1,9 +1,17 @@
 package kr.co.tjoeun.daily10minutes_20200824.fcm
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
 
 //17:MyFCMService
 class MyFCMService: FirebaseMessagingService(){
 
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
 
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+    }
 }
