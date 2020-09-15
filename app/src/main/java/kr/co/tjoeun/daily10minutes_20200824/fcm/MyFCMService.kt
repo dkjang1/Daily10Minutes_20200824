9f8db24f91b9730ad927de106bbe1b32f085d84d
JangDongKuk <dkjang@naver.com>
2020-09-15 18:03:13 +0900

    firebase-functions 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/fcm/MyFCMService.kt
@@ -0,0 +1,9 @@
+package kr.co.tjoeun.daily10minutes_20200824.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+//17:MyFCMService
+class MyFCMService: FirebaseMessagingService(){
+
+
+}
